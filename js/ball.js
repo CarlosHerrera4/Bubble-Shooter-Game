9855f7fd4e843e3e6b1bd5355dbbd0120d6f3a7a
MundoGister <mundogister@gmail.com>
2018-05-29 21:29:34 +0200

    move ball

--- js/ball.js
+++ js/ball.js
@@ -6,65 +6,80 @@ function Ball(ctx, x, y, radius) {
     this.r = this.radius;
 
     this.vx = 10;
-    this.vy = 0;
+    this.vy = 2;
 
-    this.g = 0.2;
+    //this.g = 0.2;
+}
+
+Ball.prototype.clear = function() {
+    this.ctx.clearRect(0,0,0,0);
 }
 
 Ball.prototype.setRandomColor = function () {
     this.ctx.fillStyle = "blue";
 }
 
-Ball.prototype.faster = function () {
-    this.vx *= 1.1;
-    this.vy *= 1.1;
-}
+// Ball.prototype.faster = function () {
+//     this.vx *= 1.1;
+//     this.vy *= 1.1;
+// }
 
-Ball.prototype.slower = function () {
-    this.vx *= 0.9;
-    this.vy *= 0.9;
-}
+// Ball.prototype.slower = function () {
+//     this.vx *= 0.9;
+//     this.vy *= 0.9;
+// }
 
-Ball.prototype.moveRight = function () {
-    if (this.x + this.r < this.ctx.canvas.width) {
-        this.x += this.vx;
-    }
-}
+// Ball.prototype.moveRight = function () {
+//     if (this.x + this.r < this.ctx.canvas.width) {
+//         this.x += this.vx;
+//     }
+// }
 
-Ball.prototype.moveUp = function () {
-    if (this.y - this.r > 0) {
-        this.y -= this.vy;
-    }
-}
+// Ball.prototype.moveUp = function () {
+//     if (this.y - this.r > 0) {
+//         this.y -= this.vy;
+//     }
+// }
 
-Ball.prototype.moveDown = function () {
-    if (this.y - this.r >= this.ctx.canvas.height) {
-        this.y += this.vy;
-    }
-}
+// Ball.prototype.moveDown = function () {
+//     if (this.y - this.r >= this.ctx.canvas.height) {
+//         this.y += this.vy;
+//     }
+// }
 
-Ball.prototype.moveLeft = function () {
-    if (this.x - this.r > 0) {
-        this.x -= this.vx;
-    }
-}
+// Ball.prototype.moveLeft = function () {
+//     if (this.x - this.r > 0) {
+//         this.x -= this.vx;
+//     }
+// }
 
-Ball.prototype.move = function () {
-    this.x += this.vx;
+Ball.prototype.move = function (unitX, unitY) {
+    // this.x += this.vx;
+    // this.vy += this.g;
+    // this.y += this.vy;
 
-    this.vy += this.g;
 
-    this.y += this.vy;
 
     if (this.x + this.radius > this.ctx.canvas.width ||
         this.x - this.radius < 0) {
         this.vx *= -1;
+        // this.x -= unitX;
     }
 
-    if (this.y + this.radius > this.ctx.canvas.height ||
+    else if (this.y + this.radius > this.ctx.canvas.height ||
         this.y - this.radius < 0) {
         this.vy *= -1;
     }
+    else {
+        this.x += unitX;
+        this.y -= unitY;
+
+        // this.x += this.vx;
+        // this.vy += this.g;
+        // this.y += this.vy;
+
+    }
+
 };
 
 Ball.prototype.draw = function () {
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     var canvas = document.createElement("canvas");
+    canvas.classList.add("canvas-space");
     var ctx = canvas.getContext("2d");
 
     canvas.width = 900;
@@ -8,18 +9,49 @@ window.onload = function () {
     var balls = [];
 
     // document.getElementsByClassName('spaceCanvas')[0].appendChild(canvas);
-    $('.spaceCanvas')[0].appendChild(canvas);
+    this._canvas = $('.spaceCanvas')[0].appendChild(canvas);
 
-    balls.push(new Ball(ctx, 50, 50, 20));
+    // balls.push(new Ball(ctx, 50, 50, 20));
+    balls.push(new Ball(ctx, canvas.width / 2, canvas.height - 20, 20));
+    balls[0].draw();
 
-    setInterval(function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        balls.forEach(function (ball) {
-            ball.move();
-            ball.draw();
-        });
-    }, 16);
+    $(".canvas-space").click(function (evt) {
+        var distX = (evt.offsetX - balls[0].x) / 32;
+        var distY = (balls[0].y - evt.offsetY) / 32;
+
+        var interval = setInterval(drawBall, 16);
+        function drawBall() {
+            // if ( balls[0].x === evt.offsetX || balls[0].y === evt.offsetY ) {
+            //     clearInterval(interval);
+            // }
+            if ( balls[0].y === 0){
+                clearInterval(interval);
+            }
+            else {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                balls[0].move(distX, distY);
+                balls[0].draw();
+            }
+        }
+        
+    });
+
+
+
+    // setInterval(function () {
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //     //balls[0].move();
+    //     // balls[0].draw();
+
+
+    //     // balls.forEach(function (ball) {
+    //     //     ball.move();
+    //     //     ball.draw();
+    //     // });
+    // }, 16);
 
 
 };
