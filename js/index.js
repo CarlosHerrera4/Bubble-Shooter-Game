73f76e1016ce4e915855f5c710b43f09a57dc9cb
MundoGister <mundogister@gmail.com>
2018-06-02 17:24:22 +0200

    replace rows

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ window.onload = function () {
     var canvas = document.createElement("canvas");
     canvas.classList.add("canvas-space");
     var ctx = canvas.getContext("2d");
-    canvas.width = 880;
+    canvas.width = 900;
     canvas.height = 600;
 
     // Próximas bolas
@@ -16,15 +16,30 @@ window.onload = function () {
     this._canvas = $('.spaceCanvas')[0].appendChild(canvas);
     this._canvas = $('.spaceControls')[0].appendChild(canvas2);
 
-
     // Pinto las bolas superiores
     var topBalls = [];
-    for (i = 20; i < canvas.width; i += 40) {
-        for (j = 20; j <= 220; j += 40) {
-            var topBall = new Ball(ctx, i, j, 20, 0, 0);
-            topBall.draw();
-            topBalls.push(topBall);
+    var ii = 2;
+    for (i = 20; i <= 220; i += 40) {
+        var topBall = [];
+        for (j = 20; j < canvas.width; j += 40) {
+            if (ii % 2 === 0) {
+                var _topBall = new Ball(ctx, j, i, 20, 0, 0);
+                _topBall.draw();
+                topBall.push(_topBall);
+            }
+            else {
+                var _topBall = new Ball(ctx, j + 20, i, 20, 0, 0);
+                _topBall.draw();
+                topBall.push(_topBall);
+            }
+
+
+            // var _topBall = new Ball(ctx, j, i, 20, 0, 0);
+            // _topBall.draw();
+            // topBall.push(_topBall);
         }
+        topBalls.push(topBall);
+        ii++;
     }
 
     // Pinto siguientes bolas
@@ -48,11 +63,9 @@ window.onload = function () {
 
         var interval = setInterval(drawBall, 16);
         function drawBall() {
-            // Si la bola llega a la parte superior del canvas se para el Interval
-            if ( ball.y  <= 20 ) {
-                var state = false;
-                // ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.clearRect(ball.x - 20, ball.y - 20, 40, 40);
+            if (checkIfBallHit(ball, topBalls) === true) {
+            // if (_check) {
+                // ctx.clearRect(ball.x - 20, ball.y - 20, 40, 40);
                 clearInterval(interval);
 
                 // Creamos bola nueva cuando desaparece la primera
@@ -70,15 +83,61 @@ window.onload = function () {
             }
             // Si no, la limipia y la mueve de sitio
             else {
-                ctx.clearRect(ball.x - 20, ball.y - 20, 45, 45);
+                ctx.clearRect(ball.x - 20, ball.y - 20, 40, 40);
                 ball.move();
                 ball.draw();
             }
-            function setRandomColor () {
-                var colors = ['#cc0000', '#ffff00', '#39e600', '#4d4dff', '#00ffff', '#e600e6'];
-                return colors[Math.round(Math.random() * 5)];
-            }
+
+            // Si la bola llega a la parte superior del canvas se para el Interval
+            // if ( ball.y  <= 20 ) {
+            //     ctx.clearRect(ball.x - 20, ball.y - 20, 40, 40);
+            //     clearInterval(interval);
+
+            //     // Creamos bola nueva cuando desaparece la primera
+            //     ball = new Ball(ctx, canvas.width / 2, canvas.height - 20, 20, 0, 0);
+            //     ball.color = firstBall.color;
+            //     ball.draw();
+
+            //     // Cambiamos colores a las bolas siguientes
+            //     firstBall.color = secondBall.color;
+            //     secondBall.color = thirdBall.color;
+            //     thirdBall.color = setRandomColor();
+            //     firstBall.draw();
+            //     secondBall.draw();
+            //     thirdBall.draw();
+            // }
+            // // Si no, la limipia y la mueve de sitio
+            // else {
+            //     ctx.clearRect(ball.x - 20, ball.y - 20, 45, 45);
+            //     ball.move();
+            //     ball.draw();
+            // }
+
         }
+
+        function setRandomColor() {
+            var colors = ['#cc0000', '#ffff00', '#39e600', '#4d4dff', '#00ffff', '#e600e6'];
+            return colors[Math.round(Math.random() * 5)];
+        }
+
+        function checkIfBallHit(ball, topBalls) {
+            var state = false;
+            for (i = 0; i < topBalls.length; i++) {
+                    for (j = 0; j < topBalls[i].length; j++) {
+                    console.log("i: " + i);
+                    console.log("j: " + j);
+                    a = topBalls[i][j].x - ball.x;
+                    b = topBalls[i][j].y - ball.y;
+                    var distance = Math.sqrt((a * a) + (b * b));
+                    console.log(a + ", " + b);
+                    if (distance <= 40 && state === false) {
+                        state = true;
+                    }
+                }
+            }
+            return state;
+        }
+
     });
 
 
